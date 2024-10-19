0565c583e58968959ef7e05d5307b9eaf4e61d60
Kevin Hu <kevin.hfan2023@gmail.com>
2024-10-19 01:42:39 -0700

    Start of Project

---
+++ server/server.js
@@ -0,0 +1,8 @@
+//Imports Framework
+const express = require("express");
+//Calls the express() function or basically constructor and makes instance of express().
+const app = express();
+
+const cors = require("cors");
+
+
