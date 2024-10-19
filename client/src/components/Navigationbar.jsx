ff1d86c3d1e5c73a5e6a4c9428be7cdaf65f65ce
andche742 <andche742@gmail.com>
2024-10-19 15:49:25 -0700

    added logo to nav

--- client/src/components/Navigationbar.jsx
+++ client/src/components/Navigationbar.jsx
@@ -2,27 +2,30 @@
 
 import React from 'react';
 import { Link } from 'react-router-dom';
-import styles from './componentStyle/Navbar.module.css'; // Adjusted path to component-specific CSS
+import styles from './componentStyle/Navbar.module.css'; // Ensure correct path to CSS
+import logo from '../assets/SleepingPandaLogo.png';
 
 const Navbar = () => {
   return (
+    <>
     <header className={styles.navbar}>
-    <div className={styles.logoContainer}>
-      {/* Placeholder for an image */}
-      <div className={styles.logoPlaceholder}></div>
-      <div className={styles.logo}>Do Your F'n Job</div>
-    </div>
+      <div className={styles.logoContainer}>
+        <div className={styles.logoImage}>
+          <img src={logo} alt="Logo" className={styles.logo}/>
+        </div>
+        <div className={styles.logo}>Do Your F'n Job</div>
+      </div>
 
-    {/* Auth Buttons */}
-    <div className={styles.authButtons}>
-      <Link to="/login">
-        <button className={styles.loginButton}>Log In</button>
-      </Link>
-      <Link to="/register">
-        <button className={styles.registerButton}>Register</button>
-      </Link>
-    </div>
-  </header>
+      <div className={styles.authButtons}>
+        <Link to="/login">
+          <button className={`${styles.authButtons} ${styles.loginButton}`}>Log In</button>
+        </Link>
+        <Link to="/register">
+          <button className={`${styles.authButtons} ${styles.registerButton}`}>Register</button>
+        </Link>
+      </div>
+    </header>
+    </>
   );
 };
 
