2a885898f17e360e9b688fb6dcfc145789737b3b
andche742 <andche742@gmail.com>
2024-10-19 21:02:05 -0700

    fixed login page to match register

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,13 +1,11 @@
 // pages/Login.jsx
 
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import styles from './pageStyle/login.module.css';
+import styles from './pageStyle/login.module.css'; // Import CSS
 
 export default function Login() {
   const [formData, setFormData] = useState({ email: '', password: '' });
   const [message, setMessage] = useState('');
-  const navigate = useNavigate(); //add navigation to this page.
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -24,16 +22,9 @@ export default function Login() {
       });
 
       const data = await response.json();
-      if (response.ok && data.success === true) {
-        console.log(data)
+      if (response.ok) {
+        localStorage.setItem('userEmail', formData.email);
         setMessage('Login successful!');
-
-        //Returns this under keyvalue user: {success: true, email : `${email}`}
-          //Use this: const storedUserData = JSON.parse(localStorage.getItem('user')); // storedUserData will be null if no 'user'
-
-        localStorage.setItem('user', JSON.stringify(data));
-        navigate('/tasks-page');
-
       } else {
         setMessage(data.message || 'Login failed. Please try again.');
       }
@@ -44,15 +35,12 @@ export default function Login() {
   };
 
   return (
-    <div className={styles.loginPageContainer}>
+    <div className={styles.homeContainer}>
       <div className={styles.leftSide}></div>
-
-      {/* Right side with Login title above the white box */}
       <div className={styles.rightSide}>
-        <h2 className={styles.Title}>Login</h2> {/* Title moved outside the form box */}
-        <div className={styles.loginContainer}>
-          {message && <p>{message}</p>}
-          <form onSubmit={handleSubmit}>
+        <div className={styles.formWrapper}>
+          <h2 className={styles.title}>Login</h2> {/* Title aligned above the form */}
+          <form onSubmit={handleSubmit} className={styles.form}>
             <div className={styles.formGroup}>
               <label htmlFor="email">Email:</label>
               <input
